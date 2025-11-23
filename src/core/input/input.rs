578421d3a8a1aa4092698b0d925221e982e36986
NiallWalker2012 <niallwalker2025@gmail.com>
2025-11-23 20:38:24 +0000

    Finished input.rs

--- src/core/input/input.rs
+++ src/core/input/input.rs
@@ -3,7 +3,9 @@ use crossterm::{
     event::{
         self,
         Event,
-        KeyCode
+        KeyCode,
+        KeyModifiers,
+        KeyEvent,
     },
     execute,
     style::{
@@ -26,13 +28,13 @@ use std::io::{
     Write,
     stdout
 };
+use std::time::Duration;
 
 pub struct FileConts {
     pub buffer: Vec<String>,
     pub x_pos: u32,
     pub y_pos: u32,
     pub f_name: String,     //File name
-    line: u32,
     pub status: String,
 }
 
@@ -43,7 +45,6 @@ impl FileConts {
             x_pos: 0,
             y_pos: 0,
             f_name: file_name,
-            line: 1,
             status: String::from("Ctrl-S to save, Ctrl-Q to quit"),
         }
     }
@@ -60,6 +61,9 @@ impl FileConts {
 }
 
 pub fn get_input(file_contents: String, f_name: String) -> Result<()> {
+    
+    print!("\x1B[H\x1B[2J");
+
     let mut contents = FileConts::new(f_name);
     
     if let Err(why) = contents.vectorize(file_contents) {
@@ -74,12 +78,82 @@ pub fn get_input(file_contents: String, f_name: String) -> Result<()> {
         contents.line += 1;
     }*/
     enable_raw_mode()?;
+    
+    'main: loop {
+        if let Err(why) = draw::draw(contents) {
+            eprintln!("Error whilst drawing contents: {}", why);
+            return Ok(());
+        }
 
-    if let Err(why) = draw::draw(contents) {
-        eprintln!("Error whilst drawing contents: {}", why);
-        return Ok(());
+        if event::poll(Duration::from_millis(200))? {
+            match event::read()? {
+                Event::Key(KeyEvent { code, modifiers, .. }) => {
+                    match (code, modifiers) {
+                        (KeyCode::Char('x'), KeyModifiers::CONTROL) => {
+                            if let Err(why) = exit::exit() {
+                                contents.status = format!("Couldn't exit: {}", why);
+                            }
+                        }
+                        (KeyCode::Char('s'), KeyModifiers::CONTROL) => {
+                            if let Err(why) = save::save(contents) {
+                                contents.status = format!("Failed to save contents: {}", why);
+                            }
+                        }
+                        (KeyCode::Char(input), KeyModifiers::NONE) => {
+                            if let Err(why) = insert::insert_char(contents, input) {
+                                contents.status = format!("Failed to insert char: {}", why);
+                            }
+                        }
+                        (KeyCode::Char(input), KeyModifiers::SHIFT) => {
+                            if let Err(why) = insert::insert_char(contents, input) {
+                                contents.status = format!("Failed to insert char: {}", why);
+                            }
+                        }
+                        (KeyCode::Enter, _) => {
+                            if let Err(why) = insert::insert_line(contents) {
+                                contents.status = format!("Failed to insert newline: {}", why);
+                            }
+                        }
+                        (KeyCode::Backspace, _) => {
+                            if let Err(why) = insert::backspace(contents) {
+                                contents.status = format!("Failed to backspace: {}", why);
+                            }
+                        }
+                        (KeyCode::Delete, _) => {
+                            if let Err(why) = insert::delete(contents) {
+                                contents.status = format!("Failed to delete: {}", why);
+                            }
+                        }
+                        (KeyCode::Left, _) => {
+                            if let Err(why) = cursor::move_left(contents) {
+                                contents.status = format!("Failed to move left: {}", why);
+                            }
+                        }
+                        (KeyCode::Right, _) => {
+                            if let Err(why) = cursor::move_right(contents) {
+                                contents.status = format!("Failed to move right: {}", why);
+                            }
+                        }
+                        (KeyCode::Up, _) => {
+                            if let Err(why) = cursor::move_up(contents) {
+                                contents.status = format!("Failed to move up: {}", why);
+                            }
+                        }
+                        (KeyCode::Down, _) => {
+                            if let Err(why) = cursor::move_down(contents) {
+                                contents.status = format!("Failed to move down: {}", why);
+                            }
+                        }
+                        _ => {}
+                    }
+
+                }
+            }
+        }
     }
 
+    execute!(stdout(), terminal::LeaveAlternateScreen, cursor::Show)?;
+    disable_raw_mode()?;
 
     Ok(())
 }
